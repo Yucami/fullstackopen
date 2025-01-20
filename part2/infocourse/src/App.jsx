8768c5ef69e3b0b3ea06e0e3c039895830fb7522
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-01-20 17:16:03 +0100

    courseinfo 2.5 Cambiar el componente Course a un módulo aparte

--- part2/infocourse/src/App.jsx
+++ part2/infocourse/src/App.jsx
@@ -1,52 +1,4 @@
-const Header = ({ courses }) => {
-  // console.log(course);
-  return <h1>{courses.name}</h1>;
-};
-
-const Part = ({ part }) => {
-  // console.log(part);
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  console.log(parts);
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <h3>
-      total of {" "}
-      {parts.reduce((totalExercises, part) => totalExercises + part.exercises, 0)}
-      {" "} exercises
-    </h3>
-  )
-}
-
-const Course = ({ courses }) => {
-  // console.log(course); 
-  return (
-    <div>
-      {courses.map(course => (
-        <div key={course.id}>
-          <Header courses={course} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-      ))}
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
@@ -95,6 +47,6 @@ const App = () => {
   ]
 
   return <Course courses={courses} />
-}
+};
 
-export default App
+export default App;
