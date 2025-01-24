1bebd703e8f705ffbc0894126fa6dfccb3a0b8aa
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-01-24 14:17:32 +0100

    phonebook 2.10. Extrear 3 componentes de app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,56 @@
 import { useState } from 'react';
 
+const Filter = ({ searchPerson, setSearchPerson }) => {
+  return (
+    <div>
+      filter shown with
+      <input 
+        value={searchPerson}
+        onChange={(event) => setSearchPerson(event.target.value)}
+        placeholder="Search by name"
+      />
+    </div>
+  );
+}
+
+const PersonForm = ({ addPerson, newName, setNewName, newNumber, setNewNumber }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: 
+          <input 
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+      </div>
+      <div>
+        number: 
+          <input 
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons, searchPerson }) => {
+  const personsToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(searchPerson.toLowerCase())
+  );
+  return (
+    <div>
+      {personsToShow.map(person => 
+        <div key={person.name}>{person.name} {person.number}</div>
+      )}
+    </div>
+  );
+};
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -7,6 +58,7 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 } 
   ]); 
+  
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchPerson, setSearchPerson] = useState('');
@@ -17,58 +69,24 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     } else {
       const personObject = {
-        name: newName,
+        name: newName,  
         number: newNumber,
       };
 
       setPersons(persons.concat(personObject));
       setNewName('');
-      setNewNumber(persons.concat(personObject));
       setNewNumber('');
     }
   }; 
 
-  const personsToShow = persons.filter(person => 
-    person.name.toLowerCase().includes(searchPerson.toLowerCase())
-  );
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with
-          <input 
-            value={searchPerson}
-            onChange={(event) => setSearchPerson(event.target.value)}
-            placeholder="Search by name"
-          />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: 
-            <input 
-              value={newName}
-              onChange={(event) => setNewName(event.target.value)}
-            />
-        </div>
-        <div>
-          number: 
-            <input 
-              value={newNumber}
-              onChange={(event) => setNewNumber(event.target.value)}
-            />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter searchPerson={searchPerson} setSearchPerson={setSearchPerson} />
+      <h3>Add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
       <h2>Numbers</h2>
-      <div>
-        {personsToShow.map(person => 
-          <div key={person.name}>{person.name} {person.number}</div>
-        )}
-      </div>
+      <Persons persons={persons} searchPerson={searchPerson} />
     </div>
   );
 };
