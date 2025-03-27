b9b45ff8f185b7683094dd884d0cb1acbcc87616
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-03-27 17:03:37 +0100

    part4 bloglist backend 4.9 'Testing con node:test : añadir prueba para verirficar que la propiedad id de las publicaciones sea id y no _id. Añadir método toJSON de la parte3 C'

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -19,18 +19,30 @@ beforeEach(async () => {
 })
 
 test.only('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 })
 
 test.only('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+    const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test.only('the unique identifier property og the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert(response.body.length > 0, 'No blogs found')
+
+    const firstBlog = response.body[0]
+
+    assert.ok(firstBlog.id, 'Property id is missing')
+
+    assert.strictEqual(firstBlog._id, undefined, 'Property _id should not exist')
 })
 
 after(async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 })
