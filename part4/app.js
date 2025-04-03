38b8667aa903ea57234fc85b00efff04321e2a01
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-04-03 19:28:59 +0200

    part4 bloglist backend 4.20* 'Refactorizar la función getTokenFron de blogs.js para llevar el token a un middleware para que lo puedan utilizar todas las rutas sin tner que usar la función en cada ruta'

--- part4/app.js
+++ part4/app.js
@@ -24,6 +24,7 @@ mongoose.connect(config.mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
