dc7e163cb98fba8dacd39763708b9b52f9afa6cc
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-04-09 17:54:35 +0200

    frontend notes part5 'Guardar el token en el Local Storage del navegador para que el usuario no tenga que identificarse cada vez que se refresca la página'

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -36,6 +36,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+  
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     if (!note) return;
@@ -100,6 +109,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
       noteService.setToken(user.token)
       setUser(user)
       setUsername('')
