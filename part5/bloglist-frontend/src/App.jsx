4ba2207bbc740d0e252fffdf550ecefdbb402a0f
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-04-23 18:41:02 +0200

    part5 frontend-bloglist 5.10 'Ordenar los blogs por nº de likes: método de array sort'

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -144,9 +144,11 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
 
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      ))}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => (
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+        ))}
     </div>
   )
 
